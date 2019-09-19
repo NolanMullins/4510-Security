56a949b5552b89bca7ba913a8002f0b1efc75189
NolanMullins <nolan.mullins@me.com>
2019-09-19 16:48:37 -0400

    working using pycrypto

--- a1/aes-decrypt.py
+++ a1/aes-decrypt.py
@@ -1,8 +1,32 @@
 #!/usr/bin/python3
 
 import sys
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import unpad
+
 
 if __name__ == "__main__":
-    if (len(sys.argv) <= 3):
-		print("No User specified")
+	if (len(sys.argv) <= 4):
+		print("please provide key=<128-bit key>, mode=ecb|cbc, in=<input file>, and out=<output file>")
 		exit(0)
+
+	args = {}
+	for arg in sys.argv:
+		if arg.find('=') > 0:
+			pair = arg.split('=')
+			args[pair[0]] = pair[1]
+
+	inputData = open(args['in'], "rb")
+	contents = b"".join(inputData.readlines())
+
+	if args['mode'] == "ecb":
+		cipher = AES.new(args['key'].encode("utf-8"), AES.MODE_ECB)
+	else:
+		cipher = AES.new(args['key'].encode("utf-8"), AES.MODE_CBC, b'\xb4\xb7\xf1\x9aT\xa4D\xcf1\xcaV\x0fo\xfa\x98\xc6')
+
+	dec = cipher.decrypt(contents)
+	plainText = unpad(dec, AES.block_size).decode("utf-8")
+	with open(args['out'], "w") as outData:
+		outData.write(plainText)
+
+	
--- a1/aes-encrypt.py
+++ a1/aes-encrypt.py
@@ -1,15 +1,31 @@
 #!/usr/bin/python3
 
 import sys
-from OpenSSL import SSL 
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
 
 if __name__ == "__main__":
 	if (len(sys.argv) <= 4):
 		print("please provide key=<128-bit key>, mode=ecb|cbc, in=<input file>, and out=<output file>")
 		exit(0)
 
-	print("key: " + sys.argv[1])
-	print("mode: " + sys.argv[2])
+	args = {}
+	for arg in sys.argv:
+		if arg.find('=') > 0:
+			pair = arg.split('=')
+			args[pair[0]] = pair[1]
 
+	inputData = open(args['in'], "r")
+	contents = "".join(inputData.readlines())
+	padded = pad(contents.encode("utf-8"), AES.block_size)
+	cipher = None
+
+	if args['mode'] == "ecb":
+		cipher = AES.new(args['key'].encode("utf-8"), AES.MODE_ECB)
+	else:
+		cipher = AES.new(args['key'].encode("utf-8"), AES.MODE_CBC, b'\xb4\xb7\xf1\x9aT\xa4D\xcf1\xcaV\x0fo\xfa\x98\xc6')
+	enc = cipher.encrypt(padded)
+	with open(args['out'], "wb") as outData:
+		outData.write(enc)
 
 	
